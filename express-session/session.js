795c72ec679f6182d0682dddb6f303f654b52a83
Atharv <atharvbhandare2001@gmail.com>
2021-09-20 18:21:17 +0530

    Initial commit

---
+++ express-session/session.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const session = require('express-session')
+
+const sessionOptions = (session({ secret: 'Chickens', resave: false, saveUninitialized: false }))
+app.use(sessionOptions)
+
+app.get('/viewcount', (req, res) => {
+    if (req.session.count) {
+        req.session.count += 1;
+    } else {
+        req.session.count = 1;
+    }
+    res.send(`You've viewed this page ${req.session.count} times`)
+})
+app.get('/register', (req, res) => {
+    const { username = 'Anonymous' } = req.query;
+    req.session.username = username;
+    res.redirect('/greet')
+})
+
+app.get('/greet', (req, res) => {
+    const { username } = req.session;
+    res.send(`Welcome back ${username}`)
+})
+
+app.listen(3000, () => {
+    console.log("Listening on Port 3000!")
+})
